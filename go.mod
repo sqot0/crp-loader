8bc4d3d7a3597d65982878fda5f64b699443832a
sqot0 <kuvshinov556@gmail.com>
2025-12-06 15:48:51 +0100

    add status bar

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/sqot0/crp-loader
 
 go 1.25
+
+require (
+	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/schollz/progressbar/v3 v3.18.0 // indirect
+	golang.org/x/sys v0.38.0 // indirect
+	golang.org/x/term v0.37.0 // indirect
+)
--- internal/download.go
+++ internal/download.go
@@ -4,11 +4,14 @@ import (
 	"io"
 	"net/http"
 	"os"
+
+	"github.com/schollz/progressbar/v3"
 )
 
 // DownloadFile downloads url to filepathDst (overwrites if exists)
 func DownloadFile(url, filepathDst string) error {
-	resp, err := http.Get(url)
+	req, _ := http.NewRequest("GET", url, nil)
+	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return err
 	}
@@ -20,6 +23,10 @@ func DownloadFile(url, filepathDst string) error {
 	}
 	defer func() { _ = out.Close() }()
 
-	_, err = io.Copy(out, resp.Body)
+	bar := progressbar.DefaultBytes(
+		resp.ContentLength,
+		"Установка")
+
+	_, err = io.Copy(io.MultiWriter(out, bar), resp.Body)
 	return err
 }
